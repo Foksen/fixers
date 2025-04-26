c706ff2cdc60eebacf549c95947b7f572eb1ef7f
Foksen <izholobov2004@gmail.com>
2025-04-26 13:55:07 +0300

    Add backend simple jwt auth

--- backend/fixers/users/views.py
+++ backend/fixers/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from .models import User
+from .serializers import RegisterSerializer
+from .tokens import CustomTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
