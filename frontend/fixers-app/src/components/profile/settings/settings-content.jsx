01abf8d700757ec8842ca1bf4158f62b8721bdcd
Foksen <izholobov2004@gmail.com>
2025-05-10 00:04:27 +0300

    Add warning alert on settings page

--- frontend/fixers-app/src/components/profile/settings/settings-content.jsx
+++ frontend/fixers-app/src/components/profile/settings/settings-content.jsx
@@ -1,7 +1,15 @@
 "use client";
 
 import { useState } from "react";
-import { Field, Fieldset, HStack, Stack, Text } from "@chakra-ui/react";
+import {
+  Alert,
+  Box,
+  Field,
+  Fieldset,
+  HStack,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { SidebarProfile } from "../profile-sidebar-view";
 import { SettingsActionChangeUsername } from "./actions/settings-action-change-username";
 import { SettingsActionChangePassword } from "./actions/settings-action-change-password";
@@ -12,46 +20,56 @@ export function SettingsContent({ session }) {
   const [email, setEmail] = useState(session?.user?.email);
 
   return (
-    <Stack mt="6" maxW="2xl">
-      <Fieldset.Root size="lg">
-        <Fieldset.Legend>Настройки аккаунта</Fieldset.Legend>
+    <Box>
+      <Alert.Root mt="5" status="warning" width="fit">
+        <Alert.Indicator />
+        <Alert.Title>
+          После внесения изменений для корректной работы необходимо выйти из
+          аккаунта и войти вновь
+        </Alert.Title>
+      </Alert.Root>
 
-        <Fieldset.Content mt="5" gap="5">
-          <Field.Root gap="4">
-            <Field.Label>Профиль</Field.Label>
-            <HStack w="full" justifyContent="space-between">
-              <SidebarProfile
-                size="md"
-                username={username}
-                role={session?.user?.role}
-              />
-              <SettingsActionChangeUsername
-                session={session}
-                setUsername={setUsername}
-              />
-            </HStack>
-          </Field.Root>
+      <Stack mt="6" maxW="2xl">
+        <Fieldset.Root size="lg">
+          <Fieldset.Legend>Настройки аккаунта</Fieldset.Legend>
 
-          <Field.Root gap="2">
-            <Field.Label>Почта</Field.Label>
-            <HStack w="full" justifyContent="space-between">
-              <Text>{email}</Text>
-              <SettingsActionChangeEmail
-                session={session}
-                setEmail={setEmail}
-              />
-            </HStack>
-          </Field.Root>
+          <Fieldset.Content mt="5" gap="5">
+            <Field.Root gap="4">
+              <Field.Label>Профиль</Field.Label>
+              <HStack w="full" justifyContent="space-between">
+                <SidebarProfile
+                  size="md"
+                  username={username}
+                  role={session?.user?.role}
+                />
+                <SettingsActionChangeUsername
+                  session={session}
+                  setUsername={setUsername}
+                />
+              </HStack>
+            </Field.Root>
 
-          <Field.Root gap="2">
-            <Field.Label>Пароль</Field.Label>
-            <HStack w="full" justifyContent="space-between">
-              <Text>●●●●●●●●</Text>
-              <SettingsActionChangePassword session={session} />
-            </HStack>
-          </Field.Root>
-        </Fieldset.Content>
-      </Fieldset.Root>
-    </Stack>
+            <Field.Root gap="2">
+              <Field.Label>Почта</Field.Label>
+              <HStack w="full" justifyContent="space-between">
+                <Text>{email}</Text>
+                <SettingsActionChangeEmail
+                  session={session}
+                  setEmail={setEmail}
+                />
+              </HStack>
+            </Field.Root>
+
+            <Field.Root gap="2">
+              <Field.Label>Пароль</Field.Label>
+              <HStack w="full" justifyContent="space-between">
+                <Text>●●●●●●●●</Text>
+                <SettingsActionChangePassword session={session} />
+              </HStack>
+            </Field.Root>
+          </Fieldset.Content>
+        </Fieldset.Root>
+      </Stack>
+    </Box>
   );
 }
